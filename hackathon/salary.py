88cd1485e54f4de033e9eb40f615c2ed45b5ac09
DILNA-R <100105105+DILNA-R@users.noreply.github.com>
2022-08-27 14:35:48 +0530

    Add files via upload

---
+++ hackathon/salary.py
@@ -0,0 +1,11 @@
+CTC=float(input("enter the CTC"))
+
+da=float(base*0.25)
+print(da)
+hra=float(base*0.12)
+print(hra)
+pf=float((base+da)*0.12)
+print(pf)
+other=float(CTC-(da+hra+pf))
+print(other)
+
